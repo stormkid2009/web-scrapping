63950b2d937ff10b30e5bc33281f820c68cea756
Anwar Ahmed <nileking2007@gmail.com>
2024-07-30 19:05:24 +0300

    adding comment to practise file

--- practise.py
+++ practise.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 
+# scrapping offline page with reading system file mode
 with open("mailBox.html", "r") as f:
     html_content = f.read()
 page = BeautifulSoup(html_content, "lxml")
