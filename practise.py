e6570a9f3224d3ec491e74dbcda50e626fa86d20
Anwar Ahmed <nileking2007@gmail.com>
2024-08-21 23:50:01 +0300

    deleting unecessary files

--- practise.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-
-# scrapping offline page with reading system file mode
-with open("mailBox.html", "r") as f:
-    html_content = f.read()
-page = BeautifulSoup(html_content, "lxml")
-print(page.title.text)
